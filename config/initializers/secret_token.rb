0ecd280e0385039b96cca1e8cf0e478f6e0eb674
Dan Koslow <dan.koslow@gmail.com>
2013-02-08 06:02:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecruitmentApp::Application.config.secret_token = '46f18b45f46b08d89043e5925328277fafc3a225f884bc3d2f25d77b47230827b1af03f05e37d5123090eca870c78f3efc6a6716307a9244b92df01a7d78d54e'
