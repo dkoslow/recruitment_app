10dca7e5d779cf2d9d7de342b13d43ea9370e7fa
Dan Koslow <dan.koslow@gmail.com>
2013-04-09 19:57:27 -0400

    Build out member controller actions and associated views(just test output).

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,7 @@
 class MembersController < ApplicationController
 
   before_filter :signed_in_member, only: [:index, :edit, :update, :destroy]
-  before_filter :correct_member, only: [:edit, :update]
+  before_filter :correct_member, only: [:edit, :update, :destroy]
 
   def index
     @members = Member.all
@@ -26,6 +26,7 @@ class MembersController < ApplicationController
     if @member.save
       @user.save
       sign_in @member
+      flash[:success] = "Welcome to TapConnect!"
       redirect_to @member
     else
       render 'new'
@@ -35,13 +36,31 @@ class MembersController < ApplicationController
   def edit
   end
 
+  def update
+    if @member.update_attributes(params[:member])
+      @member.user.update_attributes(company: @member.company, current_location: @member.current_location,
+                                     first_name: @member.first_name, last_name: @member.last_name,
+                                     phone_number: @member.phone_number, school: @member.school)
+      sign_in @member
+      flash[:success] = "Profile updated!"
+      redirect_to @member
+    else
+      render 'edit'
+    end
+  end
+
+
   def destroy
+    @member.destroy
+    sign_out
+    flass[:success] = "Profile destroyed!"
+    redirect_to root_url
   end
 
   private
 
     def correct_member
       @member = Member.find(params[:id])
-      redirect_to(root_url) unless current_member(@member)
+      redirect_to(root_url) unless current_member?(@member)
     end
 end
