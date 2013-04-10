213b07b890837fa4ed4e2b6dcdd9d3f9034f9302
Dan Koslow <dan.koslow@gmail.com>
2013-04-10 02:24:16 -0400

    Add relationship controller actions and edit page.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,8 +3,30 @@ class RelationshipsController < ApplicationController
   before_filter :signed_in_member
 
   def create
+    @contact = Member.find_by_id(params[:id])
+    current_member.add_contact!(@contact)
+    flash[:success] = "Contact added!"
+    redirect_to @contact
+  end
+
+  def edit
+    @contact = current_member.contacts.find_by_id(params[:id])
+  end
+
+  def update
+    @contact = current_member.contacts.find_by_id(params[:id])
+    if @contact.update_attributes(params[:contact])
+      flash[:success] = "Contact updated!"
+      redirect_to current_member
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @contact = Member.find_by_id(params[:id])
+    current_member.remove_contact!(@contact)
+    flash[:success] = "Contact removed."
+    redirect_to current_member
   end
 end
