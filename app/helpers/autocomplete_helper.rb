34fb73729e8f75f83310611368dc95f11e1ae679
Dan Koslow <dan.koslow@gmail.com>
2013-04-16 23:51:53 -0400

    Make autocomplete function more idiomatic.

--- app/helpers/autocomplete_helper.rb
+++ app/helpers/autocomplete_helper.rb
@@ -1,7 +1,7 @@
 module AutocompleteHelper
 
   def names_for(query)
-    suggestions = where("lower(name) like ? OR lower(name) like ?", "#{query.downcase}%", "% #{query.downcase}%")
+    suggestions = where("name ilike ? OR name ilike ?", "#{query}%", "% #{query}%")
     suggestions.order("popularity desc").limit(10).pluck(:name).uniq
   end
 end
